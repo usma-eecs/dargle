c61bcbc8971851e6db3363dd1c22e1e56a306141
danunn01 <42952582+danunn01@users.noreply.github.com>
2020-01-16 16:30:06 -0500

    Regex fixed

--- onion_finder.py
+++ onion_finder.py
@@ -1,15 +1,15 @@
-import re
-import warc # warc3-wet
-from glob import glob
-from time import time
-from multiprocessing import Pool, cpu_count
-from subprocess import Popen, PIPE
-from sys import platform
-from collections import Counter
+import re # Regular expression library
+import warc # warc3-wet. To read common crawl files.
+from glob import glob # To find all files in specified directory
+from time import time # Self-Explanatory
+from multiprocessing import Pool, cpu_count # To utilize multiple processors to speed up the script
+from subprocess import Popen, PIPE # Make the script universal
+from sys import platform # Determine system
+from collections import Counter # Self-Explanatory
 
 # Regex for onion domain. Must be able to find with and without www, http://, and https://
-# onion_regex = r'(?:https?://)?(?:www)?(\S*?\.onion)'
-onion_regex = r'(?:https?\:\/\/)?[\w\-\.]+\.onion'
+# onion_regex = r'(?:https?\:\/\/)?[\w\-\.]+\.onion'
+onion_domain_regex = r'(?:https?\:\/\/)?[a-zA-Z2-7]{16}\.onion'#(?:\/([^/]*))?$'
 
 # Determine OS and number of processes to use
 def os_processes():
@@ -31,27 +31,41 @@ def os_processes():
         MAX_PROCESSES = cpu_count()
     return MAX_PROCESSES
 
+# Tracker
+def track_onions():
+    pass
+
+# Counter
+def count_domains(filename):
+    with open(filename, 'r') as f:
+        with open("domain_counts.txt", 'w') as output:
+            lines = [line.strip() for line in f]
+            domain_counts = Counter(lines)
+            for k,v in  domain_counts.items():
+                output.write( "{} {}\n".format(k,v) )
 
 # Find onions in data
 def find_onions(filename):
-    onion = re.compile(onion_regex, re.IGNORECASE)
+    onion = re.compile(onion_domain_regex, re.IGNORECASE)
     with warc.open(filename) as f:
-        with open("onions.txt", 'a') as output:
+        with open("onion_domains.txt", 'a+') as output:
             for record in f:
                 url = str(record.header.get('WARC-Target-URI', None))
                 match = onion.search(url)
                 if match:
-                    print(match.group(0))
-                    output.write(match.group(0))
-                # if re.match(onion_regex, url, re.IGNORECASE):
-                    # print(url)
+                    domain = match.group(0)
+                    print(domain, file=output)
+                    # output.write(domain + '\n')
 
 if __name__ == "__main__":
     files = glob("*.warc.wet.gz")
     processors = os_processes()
-    start = time()
+    print("Searching for onions.........")
+    # start = time()
     pool = Pool(processors)
     pool.map(find_onions, files)
     pool.close()
-    end = time()
-    print("Time Elapsed: ", end - start)
+    # end = time()
+    # print("Time Elapsed: ", end - start)
+    print("Now counting domains........")
+    count_domains("onion_domains.txt")
