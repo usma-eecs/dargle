516722dc213e67dc6a788e7d41785c56fc156ff1
Yashinex <yashinexmachina@gmail.com>
2020-01-28 13:54:14 -0500

    added Dark Web request capability

--- address-verify/request.py
+++ address-verify/request.py
@@ -4,11 +4,21 @@ import requests
 outie = open('output.txt', 'w+')
 innie = open('input.txt', 'r')
 
+# socks proxies
+session = requests.session()
+session.proxies = {}
+session.proxies['http'] = 'socks5h://localhost:9050'
+session.proxies['https'] = 'socks5h://localhost:9050'
+
+# headers to throw off scent
+headers = {}
+headers['User-agent'] = "HotJava/1.1.2 FCS"
+
 # https://requests.readthedocs.io/en/master/user/quickstart/
 for line in innie:
     try:
         site = line.rstrip('\n')
-        r = requests.get(site, allow_redirects=True, timeout=10)
+        r = session.get(site, allow_redirects=True, timeout=5, headers=headers)
         # Legacy:
         #rText = str(r.text)
         rStatus = str(r.status_code)
