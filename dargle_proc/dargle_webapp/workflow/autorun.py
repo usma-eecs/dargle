659f0cde82263c1883a4dc4fac2986f3b280ec0e
Yashinex <h.strickland.e@gmail.com>
2020-03-26 19:20:09 -0400

    Added sources table

--- dargle_proc/dargle_webapp/workflow/autorun.py
+++ dargle_proc/dargle_webapp/workflow/autorun.py
@@ -3,10 +3,11 @@ import request, dargle_orm, sys
 innie = sys.argv[1]
 outie = sys.argv[2]
 # thread_num = int(sys.argv[3])
-header = sys.argv[3]
+domain = sys.argv[3]
+header = sys.argv[4]
 
 request.line_count(innie)
 
 # csv = request.process_links(innie,outie,header)
 csv = outie
-dargle_orm.dbUpdate(csv)
+dargle_orm.dbUpdate(csv,domain)
