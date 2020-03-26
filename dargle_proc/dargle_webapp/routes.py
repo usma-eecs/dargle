2694aa609737dfd2f61103320c399e30400bc533
Yashinex <h.strickland.e@gmail.com>
2020-03-26 19:29:29 -0400

    Updated Flask

--- dargle_proc/dargle_webapp/routes.py
+++ dargle_proc/dargle_webapp/routes.py
@@ -57,4 +57,17 @@ def timestamps():
     return render_template('timestamps.html', title='Timestamps', rows=pagination_rows,
                             page=page, per_page=per_page, pagination=pagination)
 
+@app.route("/domain_sources")
+def domain_sources():
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    per_page = 25
+    rows = query("sources")
+    total = len(rows)
+    pagination_rows = get_rows(rows, offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('domain_sources.html', title='Sources', rows=pagination_rows,
+                            page=page, per_page=per_page, pagination=pagination)
+
 # https://www.tutorialspoint.com/flask/flask_sqlite.htm
