a114034a422807989d02531bcce7296dfa9288f1
Derek Nunn <danunn01@gmail.com>
2020-03-26 17:48:21 -0400

    Fixed Onion Finder

--- file_parsing/Functional Scripts/onion_finder.py
+++ file_parsing/Functional Scripts/onion_finder.py
@@ -72,9 +72,8 @@ if __name__ == "__main__":
     if len(files) == 0:
         print("All Common Crawl Files have been searched!")
     else:
-        start = time()
         processors = os_processes()
+        print("Searching for onions.........")
         pool = Pool(processors)
         pool.map(find_onions, files)
         pool.close()
-        end = time()
