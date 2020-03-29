ab6776e4b289442abed28b43e19da2595dcf7d36
Derek Nunn <danunn01@gmail.com>
2020-03-29 18:56:12 -0400

    Pagination fixed?

--- dargle_proc/dargle_webapp/routes.py
+++ dargle_proc/dargle_webapp/routes.py
@@ -37,7 +37,7 @@ def domains():
     rows = query("domain")
     total = len(rows)
     pagination_rows = get_rows(rows, offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, total=total,
+    pagination = Pagination(page=page, total=total, per_page=per_page,
                             css_framework='bootstrap4')
     return render_template('domains.html', title='Domains', rows=pagination_rows,
                             page=page, per_page=per_page, pagination=pagination)
@@ -46,7 +46,6 @@ def domains():
 def timestamps():
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
-    per_page = 20
     rows = query("timestamps")
     total = len(rows)
     pagination_rows = get_rows(rows, offset=offset, per_page=per_page)
